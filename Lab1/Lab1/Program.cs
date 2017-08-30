f9dda29716a4f0c7f913e37df76d05403a9817d9
BRYAN DANIEL HATASAKA <u1028471@umail.utah.edu>
2017-08-30 10:06:42 -0600

    Just started Lab1

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Small Hello World program for Lab1
+/// </summary>
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, world"); //Prints "Hello, world" to the console
+            Console.Read(); //Keeps the program running so its possible to see the printed text
+        }
+    }
+}
