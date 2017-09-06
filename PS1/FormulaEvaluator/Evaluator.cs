4d6b21da055dddb860b5fa8d93df166031231ac2
BRYAN DANIEL HATASAKA <u1028471@umail.utah.edu>
2017-09-06 10:26:02 -0600

    Solution and projects set up

---
+++ PS1/FormulaEvaluator/Evaluator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+/// <summary>
+/// This is the formula evaluator library
+/// 
+/// Bryan Hatasaka
+/// u1028471
+/// </summary>
+namespace FormulaEvaluator
+{
+    public static class Evaluator
+    {
+        public delegate int Lookup(String v);
+
+        public static int Evaluate(String exp, Lookup variableEvaluator)
+        {
+            Stack<int> values = new Stack<int>();
+            Stack<String> operators = new Stack<String>();
+
+            string[] substrings = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
+            int parsedInt;
+
+
+            if(Int32.TryParse() )
+
+            return 0;
+        }
+    }
+
+}
