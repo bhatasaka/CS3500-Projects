2c8ab97977d76b9e1a98201ae9aec79dc3638472
BRYAN DANIEL HATASAKA <u1028471@umail.utah.edu>
2017-09-06 18:39:08 -0600

    Some basic foundation before really any algorithms are made

--- PS1/FormulaEvaluator/Evaluator.cs
+++ PS1/FormulaEvaluator/Evaluator.cs
@@ -22,15 +22,42 @@ namespace FormulaEvaluator
             Stack<int> values = new Stack<int>();
             Stack<String> operators = new Stack<String>();
 
+            //Remove all whitespace
+            exp = Regex.Replace(exp, @"\s+", "");
+
+            //Splits the exp string into an array of tokens
             string[] substrings = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
 
+            //Used if an entered token 
             int parsedInt;
 
+            //Traverses through the string
+            foreach(String substring in substrings)
+            {
+                //If the token is an integer
+                if(Int32.TryParse(substring, out parsedInt))
+                {
+                    if(operators.IsOnTop<String>("*") || operators.IsOnTop<String>("/"))
+                    {
 
-            if(Int32.TryParse() )
+                    }
+                }
+            }
+
 
             return 0;
         }
-    }
 
+
+    }
+    public static class PS1StackExtensions
+    {
+        public static bool IsOnTop<T>(this Stack<T> stack, T value)
+        {
+            if (stack.Count > 0 && stack.Peek().Equals(value))
+                return true;
+            else
+                return false;
+        }
+    }
 }
--- PS1/FormulaEvaluatorTest/EvaluatorTester.cs
+++ PS1/FormulaEvaluatorTest/EvaluatorTester.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FormulaEvaluatorTest
@@ -10,6 +11,24 @@ namespace FormulaEvaluatorTest
     {
         static void Main(string[] args)
         {
+            //Testing removing whitespace
+            String word = "This Is A String \n With Some Spaces";
+            Console.WriteLine(word);
+            word = Regex.Replace(word, @"\s+", "");
+            Console.WriteLine(word);
+
+            //Testing tryparse
+            int parsedInt;
+            Int32.TryParse("21", out parsedInt);
+
+            if (parsedInt != 21)
+                Console.WriteLine("Test 1 - Failed - parse int, regular int");
+
+
+            if (Int32.TryParse("2.1", out parsedInt))
+                Console.WriteLine("Test 2 - Failed - parse int, real number");
+
+            Console.ReadLine();
         }
     }
 }
